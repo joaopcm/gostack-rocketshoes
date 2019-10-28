97e436a856fe6277ddb3b6c62b2af157c0682d82
Joao Melo <jopcmelo@gmail.com>
2019-10-28 15:00:01 -0300

    chore: Add Home page style

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,7 +1,75 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+          alt="Items"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={20} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+          alt="Items"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={20} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+          alt="Items"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={20} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+          alt="Items"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$ 199,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={20} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }

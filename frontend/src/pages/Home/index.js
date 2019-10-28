9fb1829f2a01bc413ecbe566ed6157751c9c1b11
Joao Melo <jopcmelo@gmail.com>
2019-10-28 20:33:31 -0300

    chore: Add API request

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,75 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  async function loadProducts() {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }));
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    async function fetchProducts() {
+      await loadProducts();
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
-          alt="Items"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$ 199,00</span>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormated}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={20} color="#fff" /> 3
-          </div>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={20} color="#fff" /> 3
+            </div>
 
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
-          alt="Items"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$ 199,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={20} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
-          alt="Items"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$ 199,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={20} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
-          alt="Items"
-        />
-        <strong>Tenis muito legal</strong>
-        <span>R$ 199,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={20} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }

fcf3d439da4a922418dce3af3a6594c244c3b2ce
Joao Melo <jopcmelo@gmail.com>
2019-10-28 21:02:34 -0300

    chore: Add Reactotron + Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/reactotronConfig';
+
 import GlobalStyle from './styles/global';
-
 import Header from './components/Header';
-
 import Routes from './routes';
 
 import store from './store';
